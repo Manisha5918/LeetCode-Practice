e24614bc692c53bae0e36981b6a535234cef204d
Manisha G <manisharaj5918@gmail.com>
2026-06-29 11:32:45 +0530

    Time: 115 ms (71.56%), Space: 57.4 MB (47.92%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+       Array.Sort(nums);
+       Array.Reverse(nums);
+       
+       return nums[k-1];
+       
+    }
+}
