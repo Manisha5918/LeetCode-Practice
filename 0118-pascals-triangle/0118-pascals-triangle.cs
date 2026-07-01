f4765d6074b8f6d56c660f983720c9a8daa20867
Manisha G <manisharaj5918@gmail.com>
2026-07-01 23:45:14 +0530

    Time: 0 ms (100%), Space: 40.6 MB (26.59%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        List<IList<int>> ans = new List<IList<int>>();
+        for (int i = 0; i < numRows; i++)
+        {
+            List<int> row = new List<int>();
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0 || j == i)
+                    row.Add(1);
+                else
+                    row.Add(ans[i - 1][j - 1] + ans[i - 1][j]);
+            }
+            ans.Add(row);
+        }
+        return ans;
+    }
+}
