9429d26bf6902873588675407216909768d514cd
Manisha G <manisharaj5918@gmail.com>
2026-07-06 16:35:55 +0530

    Time: 2 ms (19.58%), Space: 31.4 MB (6.88%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsPerfectSquare(int num) {
+
+        for(long i=0;i*i<=num;i++){
+
+            if(i*i==num){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+}
