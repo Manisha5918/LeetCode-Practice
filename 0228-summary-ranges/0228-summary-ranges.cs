ac2b8fa7b75ecffcde0f80e02eca2c6f5b6e4557
Manisha G <manisharaj5918@gmail.com>
2026-07-16 23:59:33 +0530

    Time: 0 ms (100%), Space: 47.5 MB (28.24%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cs
@@ -0,0 +1,28 @@
+public class Solution
+{
+    public IList<string> SummaryRanges(int[] nums)
+    {
+        List<string> result = new List<string>();
+
+        if (nums.Length == 0)
+            return result;
+
+        int start = nums[0];
+
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            if (i == nums.Length || nums[i] != nums[i - 1] + 1)
+            {
+                if (start == nums[i - 1])
+                    result.Add(start.ToString());
+                else
+                    result.Add(start + "->" + nums[i - 1]);
+
+                if (i < nums.Length)
+                    start = nums[i];
+            }
+        }
+
+        return result;
+    }
+}
